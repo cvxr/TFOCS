4ee1c4248c5b6798ad67066babb195fe45f191d6
Stephen Becker <stephen.beckr@gmail.com>
2014-01-30 14:11:24 -0500

    Allowing proj_nuclear to take vector input as long as size variable is included

--- proj_nuclear.m
+++ proj_nuclear.m
@@ -1,4 +1,4 @@
-function op = proj_nuclear( q )
+function op = proj_nuclear( q, sz )
 
 %PROJ_NUCLEAR  Projection onto the set of matrices with nuclear norm less than or equal to q.
 %    OP = PROJ_NUCLEAR( q ) returns a function that implements the
@@ -7,6 +7,13 @@ function op = proj_nuclear( q )
 %    it must be a positive real scalar.
 %    This function is like proj_psdUTrace.m but does not assume
 %    that inputs are square Hermitian positive semidefinite matrices.
+%
+%    OP = PROJ_NUCLEAR( ..., SZ )
+%    where SZ = [n1,n2], will assume the input has been vectorized
+%    and thus will reshape it into a matrix of size n1 x n2. TFOCS
+%    can handle matrix variables so this form of PROJ_NUCLEAR is not
+%    always necessary, but it can be easier to find bugs if using
+%    only vector variables.
 %
 % This version uses a dense svd decomposition; future versions
 % of TFOCS will take advantage of low-rank and/or sparse structure.
@@ -20,10 +27,11 @@ if nargin == 0,
 elseif ~isnumeric( q ) || ~isreal( q ) || numel( q ) ~= 1 || q <= 0,
 	error( 'Argument must be positive.' );
 end
+if nargin < 2, sz = []; end
 q = proj_simplex( q );
-op = @(varargin)proj_nuclear_q( q, varargin{:} );
+op = @(varargin)proj_nuclear_q( q, sz, varargin{:} );
 
-function [ v, X ] = proj_nuclear_q( eproj, X, t )
+function [ v, X ] = proj_nuclear_q( eproj, sz, X, t )
 
 VECTORIZE = false;
 % Input must be a matrix
@@ -39,13 +47,26 @@ VECTORIZE = false;
 %     X = reshape(X, n, n );
 %     VECTORIZE   = true;
 % end
+
+% Update, Jan 2014, allowing vector variables, as long
+%   as the 'sz' variable has been given
+if ~isempty(sz)
+    if size(X,2) > 1
+        error('proj_nuclear: when using an explicit size value "sz", variable should be a vector');
+    end
+    X = reshape(X, sz(1), sz(2) );
+    VECTORIZE   = true;
+elseif size(X,2) == 1
+    warning('proj_nuclear: appears variable is a vector not a matrix. Are you sure you want the SVD of a vector?');
+end
+
 v = 0;
-if nargin > 2 && t > 0,
+if nargin > 3 && t > 0,
     [U,D,V] = svd(X,'econ');
     [dum,D] = eproj(diag(D),t);
     tt = D > 0;
     X  = U(:,tt)*diag(D(tt))*V(:,tt)';
-%     if VECTORIZE, X = X(:); end
+    if VECTORIZE, X = X(:); end
 elseif any(svd(X)<0),
     v = Inf;
 end
