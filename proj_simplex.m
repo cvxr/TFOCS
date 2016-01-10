d557bdda4b87cab079aa95089a9badc177cee76d
Stephen Becker <stephen.beckr@gmail.com>
2016-01-10 08:27:39 -0700

    Corrected/documented to allow both inequality and equality constraints

--- proj_simplex.m
+++ proj_simplex.m
@@ -1,10 +1,21 @@
-function op = proj_simplex( q )
+function op = proj_simplex( q, EQ, VECTORIZE )
 
 %PROJ_SIMPLEX	Projection onto the simplex.
 %    OP = PROJ_SIMPLEX( Q ) returns an nonsmooth function that
-%    represents the scaled simplex { x | x >= 0, sum(x) <= q }.
+%    represents the scaled simplex { x | x >= 0, sum(x) == q }.
 %    Q is optional; if not supplied, it defaults to 1. If it  is
 %    supplied, it must be a real positive scalar.
+%
+%    OP = PROJ_SIMPLEX( Q, EQ )
+%    controls whether the sum() constraint is an inequality or equality
+%    EQ = true (default) uses { x | x >= 0, sum(x) == q }.
+%    EQ = false          uses { x | x >= 0, sum(x) <= q }.
+%
+%    OP = PROJ_SIMPLEX( Q, EQ, VECTORIZE)
+%    controls how the function behaves if the input X has more than
+%    one column. If VECTORIZE=true (default), then we convert X to a 
+%    long vector. If VECTORIZE=false, then we project each column
+%    separately.
 %
 %   See also proj_psdUTrace.m (the matrix-analog of this function)
 
@@ -13,9 +24,11 @@ if nargin == 0,
 elseif ~isnumeric( q ) || ~isreal( q ) || numel( q ) ~= 1 || q <= 0,
 	error( 'Argument must be positive.' );
 end
-op = @(varargin)proj_simplex_q( q,varargin{:} );
+if nargin < 2 || isempty(EQ) , EQ = true; end % added 1/9/2016
+if nargin < 3 || isempty(VECTORIZE) , VECTORIZE = true; end % added 1/9/2016
+op = @(varargin)proj_simplex_q( q, EQ, VECTORIZE, varargin{:} );
 
-function [ v, x ] = proj_simplex_q( q, x, t )
+function [ v, x ] = proj_simplex_q( q, EQ, VECTORIZE, x, t )
 v = 0;
 
 % We have two options when input x is a matrix:
@@ -25,9 +38,10 @@ v = 0;
 % so that will be the default. However, we leave it
 % as a hard-coded option so that the user can change it
 % if they want.
-VECTORIZE = true;
+%  2016, changed to a parameter
+% VECTORIZE = true;
 
-if nargin > 2 && t > 0,
+if nargin > 4 && t > 0,
 	if any( x(:) < 0 ) || any( sum( x ) > q ),
         if VECTORIZE
             s     = sort( x(:), 'descend' );
@@ -42,13 +56,17 @@ if nargin > 2 && t > 0,
         if size(x,2) == 1 || VECTORIZE
             cs    = ( cumsum(s) - q ) ./ ( 1 : numel(s) )';
             ndx   = nnz( s > cs );
-            x     = max( x - cs(ndx), 0 );
+            tau   = cs(ndx);
+            if ~EQ, tau = max( tau, 0 ); end
+            x     = max( x - tau, 0 );
         else
             % vectorized for several columns
             cs    = diag( 1./( 1 : size(s,1) ) )*( cumsum(s) - q );
             ndx   = sum( s > cs );
             ndx   = sub2ind( size(x), ndx, 1:size(x,2)  );
-            x     = max( x - repmat(cs(ndx),size(x,1),1), 0 );
+            tau   = cs(ndx);
+            if ~EQ, tau = max( tau, 0 ); end
+            x     = max( x - repmat(tau,size(x,1),1), 0 );
         end
 	end
 % Sept 6 2012, adding factor of 100 in the line below:
