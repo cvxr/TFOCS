46ba9517d750a6f7e957b91171e220b76694b4cd
Stephen Becker <stephen.beckr@gmail.com>
2017-11-18 16:12:15 -0700

    Adding block version, faster for small n

--- prox_l1_and_sum.m
+++ prox_l1_and_sum.m
@@ -1,4 +1,4 @@
-function op = prox_l1_and_sum( q, b, nColumns, zeroID )
+function op = prox_l1_and_sum( q, b, nColumns, zeroID, useMatricized )
 
 %PROX_L1_AND_SUM    L1 norm with sum(X)=b constraints
 %    OP = PROX_L1_AND_SUM( Q ) implements the nonsmooth function
@@ -17,6 +17,11 @@ function op = prox_l1_and_sum( q, b, nColumns, zeroID )
 %    OP = PROX_L1_AND_SUM( Q, B, nColumns, zeroID )
 %       if zeroID == true (it is false by default)
 %       then after reshaping X, enforces that X(i,i) = 0
+%
+%    OP = PROX_L1_AND_SUM( Q, B, nColumns, zeroID, useMatricized )
+%       toggles between two algorithms if nColumns > 1.
+%       If useMatricized is true (default), runs a variant algorithm
+%       that is a bit faster (output should be the same).
 
 % Nov 2017, Stephen.Becker@Colorado.edu
 
@@ -29,6 +34,7 @@ if nargin < 2 || isempty(b), b = 1; else, assert( numel(b) == 1 ); end
 if nargin < 3 || isempty( nColumns), nColumns = 1;
 else assert( numel(nColumns) == 1 && nColumns >= 1 ); end
 if nargin < 4 || isempty( zeroID ), zeroID = false; end
+if nargin < 5 || isempty( useMatricized ), useMatricized = true; end
 
 if zeroID && nColumns == 1
     warning('TFOCS:prox_l1_and_sum:zeroDiag',...
@@ -36,7 +42,7 @@ if zeroID && nColumns == 1
 end
 
 % This is Matlab and Octave compatible code
-op = tfocs_prox( @(x)f(q,x), @(x,t)prox_f(q,b,nColumns,zeroID,x,t) , 'vector' );
+op = tfocs_prox( @(x)f(q,x), @(x,t)prox_f(q,b,nColumns,zeroID,useMatricized,x,t) , 'vector' );
 end
 
 % These are now subroutines, that are NOT in the same scope
@@ -44,30 +50,47 @@ function v = f(qq,x)
     v = norm( qq(:).*x(:), 1 );
 end
 
-function x = prox_f(qq,b,nColumns,zeroID,x,t) % stepsize is t
+function x = prox_f(qq,b,nColumns,zeroID,useMatricized,x,t) % stepsize is t
     tq = t .* qq; % March 2012, allowing vectorized stepsizes
     
-    if zeroID
-        if nColumns > 1
-            X   = reshape( x, [], nColumns );
+    if zeroID && nColumns > 1
+        X   = reshape( x, [], nColumns );
+        n   = size(X,1);
+        if nColumns > n
+            error('Cannot zero out the diagonal if columns > rows');
+        end
+        if useMatricized
+            Xsmall = zeros( n-1, nColumns );
             for col = 1:nColumns
-%                 x   = X(:,col);
-%                 x(col) = []; % shorten to a length n-1 x 1 vector
+                ind     = [1:col-1,col+1:size(X,1)];
+                Xsmall(:,col)   = X(ind,col);
+            end
+            Xsmall = prox_l1sum_matricized( Xsmall, tq, b );
+            for col = 1:nColumns
+                X(col,col)  = 0;
+                ind     = [1:col-1,col+1:size(X,1)];
+                X(ind,col)  = Xsmall(:,col);
+            end
+        else
+            for col = 1:nColumns
                 ind     = [1:col-1,col+1:size(X,1)];
                 x       = X(ind,col);
                 x       = prox_l1sum( x, tq, b );
                 X(col,col)  = 0;
                 X(ind,col)  = x;
             end
-            x   = X(:);
-        else
-            x   = prox_l1sum( x, tq, b );
-        end    
+        end
+        x   = X(:);
     else
         if nColumns > 1
             X   = reshape( x, [], nColumns );
-            for col = 1:nColumns
-                X(:,col) = prox_l1sum( X(:,col), tq, b );
+            
+            if useMatricized
+                X = prox_l1sum_matricized( X, tq, b );
+            else
+                for col = 1:nColumns
+                    X(:,col) = prox_l1sum( X(:,col), tq, b );
+                end
             end
             x   = X(:);
         else
@@ -105,8 +128,8 @@ function x = prox_l1sum( x0, lambda, b )
             dispp('Bounds are too close; breaking');
             break;
         end
-        j = round(mean([lwrBnd,uprBnd]));
-        printf('j is %d (bounds were [%d,%d])\n', j, lwrBnd,uprBnd );
+        j = round( (lwrBnd+uprBnd)/2 );
+        %printf('j is %d (bounds were [%d,%d])\n', j, lwrBnd,uprBnd ); %
         if j==lwrBnd
             dispp('j==lwrBnd, so increasing');
             j = j+1;
@@ -141,11 +164,11 @@ function x = prox_l1sum( x0, lambda, b )
     if lwrBnd == 0
         a2 = brk_pts( uprBnd );
         a1 = a2 - 10; % arbitrary
-        aBounds = [-Inf,a2];
+        aBounds = [a1,a2];
     elseif uprBnd == length(brk_pts) + 1
         a1 = brk_pts( lwrBnd );
         a2 = a1 + 10; % arbitrary
-        aBounds = [a1,Inf];
+        aBounds = [a1,a2];
     else
         % In general case, we can infer linear part from the two break points
         a1 = brk_pts( lwrBnd );
@@ -154,12 +177,81 @@ function x = prox_l1sum( x0, lambda, b )
     end
     
     % Now we have the support, find exact value
-    x       = xnu( mean(aBounds) );  % to find the support
+    x       = xnu(( aBounds(1)+aBounds(2))/2 );  % to find the support
     supp    = find(x);
 
     sgn     = sign(x);
     nu      = ( sum(x0(supp) - lambda*sgn(supp) ) - b )/length(supp);
     
+    x   = xnu( nu );
+
+end
+
+
+% This variant can handle several columns at once,
+% and it takes exactly log2(n) iterations, as it doesn't stop early
+%   since different columns might stop at different steps and that's
+%   not easy to detect efficiently.
+function x = prox_l1sum_matricized( x0, lambda, b )
+
+    brk_pts = sort( [x0-lambda;x0+lambda], 'descend' );
+
+    shrink  = @(x) sign(x).*max( abs(x) - lambda, 0 );
+    xnu     = @(nu) shrink( x0 - nu );
+    
+    h       = @(x) sum(x) - b; % want to solve h(nu) = 0
+
+    nCols        = size( x0, 2 ); % allow matrices
+    LDA          = size( brk_pts, 1 );
+    offsets      = (0:nCols-1)*LDA;%i.e., [0, LDA, 2*LDA, ... ];
+    
+    lwrBnd       = zeros(1,nCols);
+    uprBnd       = (length(brk_pts) + 1)*ones(1,nCols);
+    iMax         = ceil( log2(length(brk_pts)) ) + 1;
+
+    for i = 1:iMax
+
+        j = round(mean([lwrBnd;uprBnd]));
+        ind = find( j==lwrBnd );
+        j( ind ) = j( ind ) + 1;
+        ind = find( j==uprBnd );
+        j( ind ) = j( ind ) - 1;
+        
+        a   = brk_pts(j+offsets); % need the offsets to correct it here
+        x   = xnu(a);  % the prox
+        p   = h(x);
+        
+        ind = find( p > 0 );
+        uprBnd(ind) = j(ind);
+        ind = find( p < 0 );
+        lwrBnd(ind) = j(ind);
+
+    end
+
+    
+    [a1,a2]     = deal( zeros(1,nCols) );
+    ind = find( lwrBnd == 0 );
+    a2(ind) = brk_pts( uprBnd(ind) + offsets(ind) );
+    a1(ind) = a2(ind) - 10;
+    ind2 = ind;
+    
+    ind = find( uprBnd == size(brk_pts,1) + 1 );
+    a1(ind) = brk_pts( lwrBnd(ind) + offsets(ind) );
+    a2(ind) = a1(ind) + 10;
+    
+    indOther = setdiff( 1:nCols, [ind2,ind] );
+    a1(indOther)    = brk_pts( lwrBnd(indOther) + offsets(indOther) );
+    a2(indOther)    = brk_pts( uprBnd(indOther) + offsets(indOther) );
+    
+    a  = mean( [a1;a2] );
+    x       = xnu( a );
+    
+    nu      = zeros(1,nCols);
+    sgn     = sign(x);
+    for col = 1:nCols
+        supp    = find( sgn(:,col) );
+        nu(col)      = ( sum(x0(supp,col) - lambda*sgn(supp,col) ) - b )/length(supp);
+    end
     x   = xnu( nu );
 
 end
