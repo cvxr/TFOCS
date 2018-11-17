3d1e499b7fa2dfb21e3cab97b185cd9d79be9eb2
James Folberth <jamesfolberth@gmail.com>
2018-11-17 13:11:13 -0700

    Add optimized version of prox_l1_mat and a tester

---
+++ prox_l1_mat_optimized.m
@@ -0,0 +1,95 @@
+function op = prox_l1_mat_optimized( q, nColumns, zeroID)
+
+%PROX_L1_MAT    L1 norm, matricized in a special way
+%    OP = PROX_L1_MAT( Q, nColumns ) implements the nonsmooth function
+%        OP(X) = norm(Q.*X,1) with constraints 
+%    Q is optional; if omitted, Q=1 is assumed. But if Q is supplied,
+%    then it must be a positive real scalar (or must be same size as X).
+%
+%    OP = PROX_L1_MAT( Q, nColumns )
+%       takes the input vector X and reshapes it to size [nRows nColumns]
+%       and applies this prox to every column
+%
+%    OP = PROX_L1_MAT( Q, nColumns, zeroID )
+%       if zeroID == true (it is false by default)
+%       then after reshaping X, enforces that X(i,i) = 0
+%
+% Often useful for sparse subpsace clustering (SSC)
+%   See, e.g., https://github.com/stephenbeckr/SSC
+
+% Mar 2018, Stephen.Becker@Colorado.edu
+
+if nargin == 0
+    q = 1;
+elseif ~isnumeric( q ) || ~isreal( q ) ||  any( q < 0 ) || all(q==0) || numel( q ) ~= 1
+    error( 'Argument must be positive.' );
+end
+
+if nargin < 2 || isempty(nColumns)
+    nColumns = 1;
+else
+    assert( numel(nColumns) == 1 && nColumns >= 1 );
+end
+
+if nargin < 3 || isempty( zeroID ), zeroID = false; end
+
+if zeroID && nColumns == 1
+    warning('TFOCS:prox_l1_mat:zeroDiag',...
+        'You requested enforcing zero diagonals but did not set nColumns>1 which is probably a mistake');
+end
+
+
+% 3/15/18, adding:
+%JMF 17 Nov 2018: determine if we have shrink_mex once when constructing the prox handle
+if 3~=exist('shrink_mex','file')
+    addpath( fullfile( tfocs_where, 'mexFiles' ) );
+end
+if 3==exist('shrink_mex','file') 
+    shrink  = @(x,tq) shrink_mex(x,tq);
+else
+    % this is fast, but requires more memory
+    shrink  = @(x,tq) sign(x).*max( abs(x) - tq, 0 );
+end
+
+
+% This is Matlab and Octave compatible code
+op = tfocs_prox( @(x)f(q,x), @(x,t)prox_f(q,nColumns,zeroID,shrink,x,t) , 'vector' );
+
+end
+
+% These are now subroutines, that are NOT in the same scope
+function v = f(qq,x)
+    v = norm( qq(:).*x(:), 1 );
+end
+
+function x = prox_f(qq,nColumns,zeroID,shrink,x,t) % stepsize is t
+    tq = t .* qq; % March 2012, allowing vectorized stepsizes
+    
+    if zeroID && nColumns > 1
+        x = reshape( x, [], nColumns );
+        nRows = size(x,1);
+        if nColumns > nRows
+            error('Cannot zero out the diagonal if columns > rows');
+        end
+
+        x = shrink(x, tq);
+
+        diag_inds = nRows*(0:nRows-1) + (1:nRows);
+        x(diag_inds) = 0;
+
+        x = reshape(x, nRows*nColumns, 1);
+
+    else
+        if nColumns > 1
+            x = reshape( x, [], nColumns );
+            nRows = size(x,1);
+
+            x = shrink(x, tq);
+
+            x = reshape(x, nRows*nColumns, 1);
+        else
+            x   = shrink(x, tq);
+        end
+    end
+end
+
---
+++ tests/test_prox_l1_mat.m
@@ -0,0 +1,76 @@
+function [] = test_prox_l1_mat()
+    
+    check_correctness()
+    time_single_run()
+
+end
+
+function [] = check_correctness()
+% Check correctness of optimizations done to prox_l1_mat
+
+    % An example from sparse subspace clustering (SSC)
+    n_rows = 6000;
+    n_cols = 6000;
+    x = randn(n_rows, n_cols);
+    x = x(:);
+    t = rand(n_cols,1);
+    Q = 1;
+    zeroID = true;
+    useMatricized = true;
+    
+    % Make the prox operators
+    prox_ref = prox_l1_mat(Q, n_cols, zeroID, useMatricized);
+    prox_test = prox_l1_mat_optimized(Q, n_cols, zeroID);
+    
+    y_ref = prox_ref(x, t);
+    y_test = prox_test(x, t);
+
+    rel_err = norm(y_ref - y_test, 'fro') / norm(y_ref, 'fro');
+    fprintf('relative error = %1.5e\n', rel_err);
+
+end
+
+
+function [] = time_single_run()
+% Time prox_l1_mat implementations
+
+    minimum_runtime = 1;
+    
+    % An example from sparse subspace clustering (SSC)
+    n_cols = 6000;
+    x = randn(n_cols, n_cols);
+    x = x(:);
+    t = 1;
+    Q = 1;
+    zeroID = true;
+    useMatricized = true;
+    
+    % Make the prox operator
+    prox = prox_l1_mat(Q, n_cols, zeroID, useMatricized);
+    %prox = prox_l1_mat_optimized(Q, n_cols, zeroID);
+    
+    % Warm up
+    n_done = 0;
+    t_ = tic();
+    while true
+        if toc(t_) >= minimum_runtime
+            break
+        end
+
+        y = prox(x, t);
+        n_done = n_done + 1;
+    end
+
+    % Measure runtime
+    times = zeros(n_done,1);
+    for n=1:n_done
+        t_ = tic();
+        y = prox(x, t);
+        times(n) = toc(t_);
+    end
+
+    fprintf('prox min/mean/max runtime = %1.5e  %1.5e  %1.5e  seconds\n', min(times), mean(times), max(times));
+
+end
+
+
